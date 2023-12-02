851203c01e6237011fb0fe48d0b8d1ff08246066
kelvinTest05 <testkelvin05@gmail.com>
2023-12-02 23:49:09 +0530

    new commit

--- src/Config/AxiosInstance.js
+++ src/Config/AxiosInstance.js
@@ -8,7 +8,7 @@ const AxiosInstance=axios.create({
 AxiosInstance.interceptors.request.use(function(config){
     const token =localStorage.getItem('token')
     config.headers['Authorization']='Bearer '+token
-    config.headers['Access-control-Allow-Origin']='*'
+    // config.headers['Access-control-Allow-Origin']='*'
     return config
 })
 
